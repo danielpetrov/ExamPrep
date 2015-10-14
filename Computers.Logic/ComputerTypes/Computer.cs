83bfb2eac9c83fb04300147ec47fc97ea9c873ee
danielpetrov <danielpetrov666@mail.bg>
2015-10-14 11:30:48 +0300

    Logic lib

---
+++ Computers.Logic/ComputerTypes/Computer.cs
@@ -0,0 +1,27 @@
+﻿namespace Computers.UI
+{
+    using System.Collections.Generic;
+
+    public abstract class Computer
+    {
+        internal Computer(
+            Cpu cpu, 
+            Ram ram, 
+            IEnumerable<HardDriver> hardDrives, 
+            VideoCard videoCard)
+        {
+            this.Cpu = cpu;
+            this.Ram = ram;
+            this.HardDrives = hardDrives;
+            this.VideoCard = videoCard;
+        }
+
+        protected IEnumerable<HardDriver> HardDrives { get; set; }
+       
+        protected VideoCard VideoCard { get; set; }
+        
+        protected Cpu Cpu { get; set; }
+       
+        protected Ram Ram { get; set; }
+     }
+}
